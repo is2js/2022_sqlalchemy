b680c1cf72d2be8b564ab90bbf924947745f89c6
is2js <tingstyle1@gmail.com>
2023-02-09 06:12:13 +0900

    feat: 통계 sql문을 dialect에 따라 다르게 작성(오타 수정, postgre alias에 ' ' 단일따옴표 제거, to_date추가, interval구문을 수정)

--- src/main/routes/admin_route.py
+++ src/main/routes/admin_route.py
@@ -345,7 +345,7 @@ def generate_series_subquery(db, start_date, end_date, interval='day'):
         select_date = f"SELECT date(date, '+1 {interval}')  AS 'date'"
         to_char = f"strftime('{strftime_format}', date)"
     elif isinstance(db.session.bind.dialect, postgresql.dialect):
-        select_date = f"SELECT to_date(date, 'yyyy-mm-dd') + interval '1 {interval}s' AS date" # postgre는 '단일따옴표 안붙인다.
+        select_date = f"SELECT to_date(date, 'yyyy-MM-dd') + interval '1 {interval}s' AS date" # postgre는 '단일따옴표 안붙인다.
         to_char = f"TO_CHAR(date, '{strftime_format}')"
     elif isinstance(db.session.bind.dialect, mysql.dialect):
         select_date =  f"SELECT date + interval 1 {interval}"
@@ -367,6 +367,7 @@ def generate_series_subquery(db, start_date, end_date, interval='day'):
             
             SELECT {to_char} FROM dates
             """
+    # stmt = f"""select to_char(generate_series, 'yyyy-MM-dd') as date form generate_series(:start_date::DATE, :end_date::DATE, '1 {interval}'::INTERVAL)"""
     _text = text(
         stmt
     ).bindparams(start_date=to_string_date(start_date), end_date=to_string_date(end_date))
