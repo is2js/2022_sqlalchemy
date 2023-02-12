bfc01dce8efd745d544a2ac094a342460a5eebc1
is2js <tingstyle1@gmail.com>
2023-02-12 15:03:12 +0900

    fix: flask shell에 session을 쓸 수 있도록 코드 수정

--- manage.py
+++ manage.py
@@ -1,7 +1,8 @@
 import os
 
 from src.infra.config.connection import DBConnectionHandler
-from src.infra.tutorial3 import User, Role
+from src.infra.config.query import StaticQuery
+from src.infra.tutorial3 import *
 from src.main.config import create_app
 
 app = create_app(os.getenv('APP_CONFIG') or 'default')
@@ -10,4 +11,10 @@ app = create_app(os.getenv('APP_CONFIG') or 'default')
 # flask shell에 객체들을 미리 던져놓는다.
 @app.shell_context_processor
 def make_shell_context():
-    return dict(db=DBConnectionHandler(), User=User, Role=Role)
+    db = DBConnectionHandler()
+    Session = db.get_current_session()
+    db.session = Session()
+
+    return dict(db=db,
+                User=User, Role=Role, Post=Post, Category=Category,
+                StaticQuery=StaticQuery)
