d583c62bb4e2fd90e8811d9a41f32e33c8353e5a
is2js <tingstyle1@gmail.com>
2023-03-29 21:26:48 +0900

    feat: todo axios  수정 구현

--- src/main/routes/todo_route.py
+++ src/main/routes/todo_route.py
@@ -20,6 +20,7 @@ def all(employee_id):
 def add():
     payload = request.get_json()
     # payload  >>  {'employee_id': 1, 'type': '개인', 'todo': 'fasdfsa', 'target_date': None}
+    # payload  >>  {'employee_id': 1, 'type': 0, 'todo': 'ㅇㅇㅇㅇㅇㅇ', 'target_date': '2023. 3. 31.'}
 
     payload['todo'] = payload['todo'].strip()
     payload['target_date'] = parse(payload['target_date'])
@@ -32,26 +33,31 @@ def add():
         return make_response(dict(new_todo=new_todo, message='할일 등록 완료!'), 201)
     else:
         return make_response(dict(message='댓글 등록 실패!'), 409)
-#
-#
-# @comment_bp.route("/edit", methods=['PUT'])
-# def edit():
-#     payload = request.get_json()
-#     comment = Comment.filter_by(id=payload['id']).first()
-#     if not comment:
-#         return make_response(dict(message='해당 댓글이 존재하지 않습니다'), 409)
-#
-#     result, msg = comment.update(content=payload['content'])
-#
-#     if result:
-#         updated_comment = result.to_dict2(nested=Comment._MAX_LEVEL, relations='replies',
-#                                           hybrid_attrs=True,
-#                                           exclude=['path', 'sort'])
-#
-#         return make_response(dict(updated_comment=updated_comment, message="댓글이 수정 완료."), 200)
-#     else:
-#         return make_response(dict(message="댓글 수정 실패"), 409)
-#
+
+@todo_bp.route("/edit", methods=['PUT'])
+def edit():
+    payload = request.get_json()
+    # print('payload  >> ', payload)
+    # payload  >>  {'id': 1, 'type': 1, 'todo': 'ㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋ2', 'target_date': '2023-03-29T15:00:00.000Z'}
+    # payload  >>  {'id': 32, 'type': 1, 'todo': 'ㅇㅇㅇ', 'target_date': '2023. 3. 28.'}
+
+    todo = Todo.filter_by(id=payload['id']).first()
+    if not todo:
+        return make_response(dict(message='해당 할일이 존재하지 않습니다'), 409)
+
+    result, msg = todo.update(
+        todo=payload['todo'].strip(),
+        type=payload['type'],
+        target_date=parse(payload['target_date']),
+    )
+
+    if result:
+        updated_todo = result.to_dict2(hybrid_attrs=True)
+
+        return make_response(dict(updated_todo=updated_todo, message="할 일 수정 완료."), 200)
+    else:
+        return make_response(dict(message="할 일 수정 실패"), 409)
+
 #
 # @comment_bp.route("/blind", methods=['PUT'])
 # def blind():
